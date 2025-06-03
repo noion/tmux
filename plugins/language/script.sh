c45a037c807aae519d5b24e4aff9d045a0e3da4f
noion <n3t1m3n@gmail.com>
2025-06-03 20:49:16 +0300

    Delete plugins/language directory

--- plugins/language/script.sh
+++
@@ -1,3 +0,0 @@
-#!usr/bin/env bash
-
-defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID | awk -F. '{ if ($4 == "ABC") print "EN"; else print "RU" }'
